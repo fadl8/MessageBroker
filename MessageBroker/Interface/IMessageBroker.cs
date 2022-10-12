88c9c558877cb5f9f2c1718b36a89f3218ccba51
fadl8 <fadhl2020nasser@gmail.com>
2022-10-12 02:38:50 -0700

    add main massage border

---
+++ MessageBroker/Interface/IMessageBroker.cs
@@ -0,0 +1,16 @@
+﻿using MessageBroker.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageBroker.Interface
+{
+    public interface IMessageBroker : IDisposable
+    {
+        void Publish<T>(object source, T message);
+        void Subscribe<T>(Action<MessagePayload<T>> subscription);
+        void Unsubscribe<T>(Action<MessagePayload<T>> subscription);
+    }
+}
---
+++ MessageBroker/Model/MessagePayload.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageBroker.Model
+{
+    public class MessagePayload<T>
+    {
+        public object Who { get; private set; }
+        public T What { get; private set; }
+        public DateTime When { get; private set; }
+        public MessagePayload(T payload, object source)
+        {
+            Who = source; What = payload; When = DateTime.UtcNow;
+        }
+    }
+}
